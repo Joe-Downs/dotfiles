1767cd27b28d895829f1c0631bdaa0e662c8e203
Joe Downs <joe@dwns.dev>
2025-07-01 13:59:45 -0600

    Use helm-buffers-list instead of the stock Helm buffer switcher

--- emacs/cfg-helm.el
+++ emacs/cfg-helm.el
@@ -7,6 +7,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
