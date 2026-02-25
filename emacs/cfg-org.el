3b263658989af6ac3025a8e50c4a264089249194
Joe Downs <joe@dwns.dev>
2026-02-25 00:02:23 -0500

    Add more times for org-mode effort estimation

--- emacs/cfg-org.el
+++ emacs/cfg-org.el
@@ -33,8 +33,9 @@
                                       "  /Entered on/ %U"))
                             ))
    (org-directory org-dir)
-   (org-global-properties
-    '(("Effort_ALL" . "0:00 0:05 0:10 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 7:00")))
+   (org-global-properties '(("Effort_ALL" . "0:00 0:05 0:10 0:15 0:30 0:45 \
+                                             1:00 2:00 3:00 4:00 5:00 6:00 7:00 \
+                                             8:00 12:00 16:00 24:00 32:00 40:00")))
    (org-log-done 'time)
    (org-outline-path-complete-in-steps nil)
    (org-refile-targets '((org-agenda-files :maxlevel . 2)))
