1a517bfa2b20f2fa3bb2b0b639dcbf853da1ebc6
Joe Downs <joe@dwns.dev>
2026-09-22 21:30:25 -0400

    Add keybind for upcase-initials-region

--- emacs/keybinds.el
+++ emacs/keybinds.el
@@ -8,3 +8,4 @@
 (global-set-key (kbd "C-c s n") 'yas-new-snippet)
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c \"") 'poporg-dwim)
+(global-set-key (kbd "C-x C-i") 'upcase-initials-region)
